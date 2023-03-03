32b7df0548df227bb5552aff153b26ec4b0bd286
Abhishekranjan8051 <abhishekranjan8051@gmail.com>
2023-03-03 15:00:21 +0530

    flatlist and touchable opacity

--- App.js
+++ App.js
@@ -1,16 +1,85 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const [todo, setTodo] = useState("");
-
+  const [people, setPeople] = useState([
+    {
+      name: "Vinte",
+      key: 1,
+    },
+    {
+      name: "Oloo",
+      key: 2,
+    },
+    {
+      name: "Thoughtblab",
+      key: 3,
+    },
+    {
+      name: "Dabfeed",
+      key: 4,
+    },
+    {
+      name: "Yotz",
+      key: 5,
+    },
+    {
+      name: "Meejo",
+      key: 6,
+    },
+    {
+      name: "Skilith",
+      key: 7,
+    },
+    {
+      name: "Skippad",
+      key: 8,
+    },
+    {
+      name: "Devpoint",
+      key: 9,
+    },
+    {
+      name: "Ozu",
+      key: 10,
+    },
+  ]);
   const clickHandler = () => {
     setTodo("abhishek");
   };
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prev) => {
+      return prev.filter((person) => person.key != id);
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>My Todo</Text>
+      <FlatList
+        numColumns={2}
+        data={people}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => (
+          <View>
+            <Text></Text>
+            <TouchableOpacity onPress={() => pressHandler(item.key)}>
+              <Text style={styles.input}>{item.name}</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+
+      {/* <Text>My Todo</Text>
       <Text></Text>
       <Text>{todo}</Text>
       <Text></Text>
@@ -25,7 +94,7 @@ export default function App() {
       <View>
         <Button title="Add Todo" onPress={clickHandler} />
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -38,11 +107,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   input: {
-    borderColor: "#777",
+    borderColor: "green",
     borderWidth: 1,
     padding: 5,
     margin: 10,
-    width: 200,
+    width: 150,
+    marginTop: 30,
+    backgroundColor: "pink",
   },
   text: {
     margin: "auto",
